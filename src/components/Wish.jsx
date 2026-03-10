ecafdefec20068639eb65405f652ae8c0bc880f4
karthi3075 <karthijayakumar3007@gmail.com>
2026-03-10 23:28:46 +0530

    Add interactive wish response functionality

--- src/components/Wish.jsx
+++ src/components/Wish.jsx
@@ -1,17 +1,50 @@
 import ReactConfetti from "react-confetti";
 
 export default function Wish(){
-    return(
-        <div className="h-screen flex justify-center items-center w-screen bg-gray-500">
-            <ReactConfetti/>
-            <div className="p-3 bg-blue-100 rounded-md flex flex-col gap-3 items-center">
-                <img src="/images.jpeg" alt="Your photo is loading" />
-                <audio controls autoPlay loop>
-                    <source src="/song.mp3" type="audio/mpeg" autoPlay/>
-                </audio>
-                <h1 className="text-2xl font-bold text-center text-blue-400">Happy Birth Day</h1>
+     const [no, setNo] = useState(false);
+    const [yes, setYes] = useState(false);
+    const [msg, setMsg] = useState("");
+    const handleNo = () => {
+        setNo(true)
+        setYes(false)
+        setMsg("Poi Yes Click Pannu")
+    }
+    const handleYes = () => {
+        setYes(true);
+        setNo(false)
+        setMsg("Give Me The Treat")
+    }
+
+    const handleClick=()=>{
+        setYes(false);
+        setNo(false);
+    }
+    return (
+        <div className="bg-gray-300"> 
+            <div className="flex justify-center flex-col items-center w-screem h-screen">
+                {(no || yes) &&
+                    <div className="m-5 ">
+                        <div className={`flex relative font-semibold md:text-2xl px-5 pr-10 ${yes ? "text-green-500 bg-green-300 " : "text-red-500 bg-red-100"} `}>
+                            <p>{msg}</p>
+                             <button className="absolute right-0 top-0 bottom-0 px-3 mb-2 flex items-center cursor-pointer text-red-500" onClick={handleClick}>x</button>
+                        </div>
+                    </div>
+                }
+                <ReactConfetti/>
+                <div className="bg-blue-300 p-5 rounded text-white">
+                    <div className=" text-center font-bold">
+                        <h1 className="text-3xl bg-gradient-to-r from-red-500 via-yellow-500 to-blue-500 bg-[length:200% 200%] bg-clip-text text-transparent animate-gradient animate-pulse">Happy Birth Day</h1>
+                        <h3 className="text-2xl mt-3  bg-gradient-to-r from-red-500 via-yellow-500 to-blue-500 bg-[length:200% 200%] bg-clip-text text-transparent animate-gradient animate-pulse">Friend</h3>
+                    </div>
+                    <div className="mt-4">
+                        <p className="mb-4 text-center">are you accept my wish</p>
+                        <div className="flex justify-between">
+                            <button className="px-4 py-1 bg-green-500 rounded cursor-pointer" onClick={handleYes}>Yes</button>
+                            <button className="px-4 py-1 bg-red-500 rounded cursor-pointer hover:translate-20 duration-400" onClick={handleNo}>No</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        
         </div>
     )
 }
